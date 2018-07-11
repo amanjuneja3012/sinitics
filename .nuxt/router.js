0204680dbea0f0818d362ae7ae7f0b7cbf707a4d
amanjuneja3012 <amanjuneja3012@gmail.com>
2018-07-11 15:17:38 +0530

    fixes for locale integrations

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,18 +3,18 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-const _0e7cac7f = () => import('../pages/pricing.vue' /* webpackChunkName: "pages/pricing" */).then(m => m.default || m)
-const _e8301ecc = () => import('../pages/botic.vue' /* webpackChunkName: "pages/botic" */).then(m => m.default || m)
-const _83c26df4 = () => import('../pages/about.vue' /* webpackChunkName: "pages/about" */).then(m => m.default || m)
-const _4452b86a = () => import('../pages/index.vue' /* webpackChunkName: "pages/index" */).then(m => m.default || m)
-const _d8b3eb6e = () => import('../pages/_lang/index.vue' /* webpackChunkName: "pages/_lang/index" */).then(m => m.default || m)
-const _e3e72906 = () => import('../pages/_lang/pricing.vue' /* webpackChunkName: "pages/_lang/pricing" */).then(m => m.default || m)
-const _41b75718 = () => import('../pages/_lang/botic.vue' /* webpackChunkName: "pages/_lang/botic" */).then(m => m.default || m)
-const _73ee2f84 = () => import('../pages/_lang/about.vue' /* webpackChunkName: "pages/_lang/about" */).then(m => m.default || m)
+const _da30d96e = () => import('../pages/pricing.vue' /* webpackChunkName: "pages/pricing" */).then(m => m.default || m)
+const _129201e4 = () => import('../pages/botic.vue' /* webpackChunkName: "pages/botic" */).then(m => m.default || m)
+const _44c8da50 = () => import('../pages/about.vue' /* webpackChunkName: "pages/about" */).then(m => m.default || m)
+const _6480b515 = () => import('../pages/index.vue' /* webpackChunkName: "pages/index" */).then(m => m.default || m)
+const _f16707da = () => import('../pages/_lang/index.vue' /* webpackChunkName: "pages/_lang/index" */).then(m => m.default || m)
+const _31dd92c7 = () => import('../pages/_lang/pricing.vue' /* webpackChunkName: "pages/_lang/pricing" */).then(m => m.default || m)
+const _355dc8e2 = () => import('../pages/_lang/botic.vue' /* webpackChunkName: "pages/_lang/botic" */).then(m => m.default || m)
+const _6794a14e = () => import('../pages/_lang/about.vue' /* webpackChunkName: "pages/_lang/about" */).then(m => m.default || m)
 
 
 
-const scrollBehavior = function(to, from, savedPosition) {
+const scrollBehavior = function (to, from, savedPosition) {
       if (to.hash) {
           return {selector: to.hash}
       } else {
@@ -33,42 +33,42 @@ export function createRouter () {
     routes: [
 		{
 			path: "/pricing",
-			component: _0e7cac7f,
+			component: _da30d96e,
 			name: "pricing"
 		},
 		{
 			path: "/botic",
-			component: _e8301ecc,
+			component: _129201e4,
 			name: "botic"
 		},
 		{
 			path: "/about",
-			component: _83c26df4,
+			component: _44c8da50,
 			name: "about"
 		},
 		{
 			path: "/",
-			component: _4452b86a,
+			component: _6480b515,
 			name: "index"
 		},
 		{
 			path: "/:lang",
-			component: _d8b3eb6e,
+			component: _f16707da,
 			name: "lang"
 		},
 		{
 			path: "/:lang/pricing",
-			component: _e3e72906,
+			component: _31dd92c7,
 			name: "lang-pricing"
 		},
 		{
 			path: "/:lang/botic",
-			component: _41b75718,
+			component: _355dc8e2,
 			name: "lang-botic"
 		},
 		{
 			path: "/:lang/about",
-			component: _73ee2f84,
+			component: _6794a14e,
 			name: "lang-about"
 		}
     ],
--- plugins/i18n.js
+++ plugins/i18n.js
@@ -3,6 +3,14 @@ import VueI18n from 'vue-i18n'
 
 Vue.use(VueI18n)
 
+
+function extractLocaleJson(locale){
+  var keys = require('~/locales/translationKeys.json');
+  var map = require('~/locales/translations.json');
+  var data = map[locale];
+  return iterateAndFillDynamicData(keys,data);
+}
+
 export default ({ app, store }) => {
   // Set i18n instance on app
   // This way we can use it in middleware and pages asyncData/fetch
@@ -10,8 +18,8 @@ export default ({ app, store }) => {
     locale: store.state.locale,
     fallbackLocale: 'en',
     messages: {
-      'en': require('~/locales/en.json'),
-      'fr': require('~/locales/fr.json')
+      'en': extractLocaleJson('en'),
+      'fr': extractLocaleJson('en')
     }
   })
 
@@ -23,4 +31,55 @@ export default ({ app, store }) => {
       return `/${app.i18n.locale}/${link}`
     }
   }
+}
+
+function iterateAndFillDynamicData(item,dataObj) {
+    if (!item) { return item; } // null, undefined values check
+
+    var types = [String], 
+        result;
+
+    // normalizing primitives if someone did new String('aaa'), or new Number('444');
+    types.forEach(function(type) {
+        if (typeof(item)=='string') {
+            result = dataObj[item];
+        }
+    });
+
+    if (typeof result == "undefined") {
+        if (Object.prototype.toString.call( item ) === "[object Array]") {
+            result = [];
+            item.forEach(function(child, index, array) { 
+                result[index] = iterateAndFillDynamicData( child ,dataObj);
+            });
+        } else if (typeof item == "object") {
+            // testing that this is DOM
+            if (item.nodeType && typeof item.cloneNode == "function") {
+                result = item.cloneNode( true );    
+            } else if (!item.prototype) { // check that this is a literal
+                if (item instanceof Date) {
+                    result = new Date(item);
+                } else {
+                    // it is an object literal
+                    result = {};
+                    for (var i in item) {
+                        result[i] = iterateAndFillDynamicData( item[i] ,dataObj);
+                    }
+                }
+            } else {
+                // depending what you would like here,
+                // just keep the reference, or create new object
+                if (false && item.constructor) {
+                    // would not advice to do that, reason? Read below
+                    result = new item.constructor();
+                } else {
+                    result = item;
+                }
+            }
+        } else {
+            result = item;
+        }
+    }
+
+    return result;
 }
